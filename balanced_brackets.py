6b21d08bb1fa58a2f3896f0de161d0547f982abd
Nadyan <nadyan_sp@hotmail.com>
2022-07-11 16:28:07 -0300

    feat: verifies if a string of brackets is balanced

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -1,11 +1,44 @@
 #!/bin/python3
 
+from typing import List
+
+OPEN_BRACKETS = ['(', '[', '{']
+
 # Complete the 'isBalanced' function below.
 #
 # The function is expected to return a STRING.
 # The function accepts STRING brackets as parameter.
-def isBalanced(brackets):
-    # Write your code here
+def isBalanced(brackets: str) -> str:
+    """ Verifies if a string of brackets is balanced
+        :params - brackets: str
+        :return - 'YES' or 'NO': str
+    """
+
+    # keeps track of the open brackets,
+    # acts like a stack
+    open: List[str] = []
+    
+    for bracket in brackets:
+        if bracket in OPEN_BRACKETS:
+            # puts the open bracket in the stack
+            open.append(bracket)
+        else:
+            # for each close bracket we need a 
+            # matching open bracket on the top of the stack.
+            open_to_match = open.pop()
+            if ((open_to_match == '(' and bracket != ')') 
+                    or (open_to_match == '[' and bracket != ']')
+                    or (open_to_match == '{' and bracket != '}')):
+                # if the closing bracket does not match the open bracket
+                # on the top of the stack, its not balanced
+                return 'NO'
+    
+    if len(open) != 0:
+        # after all the iterations, if there is still
+        # an open bracket in the stack, its not balanced
+        return 'NO'
+
+    return 'YES'
 
 
 if __name__ == '__main__':
